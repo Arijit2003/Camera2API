552debc7fde726b522955e67e135fa00ecf7addd
Arijit2003 <arijitmodak2003@gmail.com>
2023-06-20 00:21:59 +0530

    dimens, surface_texture and capture_btn

--- app/src/main/java/com/example/camera2api/MainActivity.java
+++ app/src/main/java/com/example/camera2api/MainActivity.java
@@ -3,16 +3,25 @@ package com.example.camera2api;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.content.DialogInterface;
+import android.graphics.SurfaceTexture;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.TextureView;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+    private FloatingActionButton captureBtn;
+    private TextureView textureView;
+
+
     ActivityResultLauncher<String> cameraResultLauncher= registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
         @Override
         public void onActivityResult(Boolean result) {
@@ -29,8 +38,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         cameraResultLauncher.launch(Manifest.permission.CAMERA);
+        init();
     }
-    public void showAlertDialog(String title,String message){
+
+    private void init(){
+        captureBtn=findViewById(R.id.captureBtn);
+        textureView=findViewById(R.id.textureView);
+    }
+
+
+    public  void showAlertDialog(String title,String message){
         AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
         alertDialog.setMessage(message);
         alertDialog.setTitle(title);
